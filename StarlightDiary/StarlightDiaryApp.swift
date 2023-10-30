3ca327f06c651a1737cc1738f26e3787b2a37aab
wxm <w05172075@outlook.com>
2023-10-30 18:40:50 +0800

    Initial Commit

---
+++ StarlightDiary/StarlightDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarlightDiaryApp.swift
+//  StarlightDiary
+//
+//  Created by 萌萌 on 2023/10/30.
+//
+
+import SwiftUI
+
+@main
+struct StarlightDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
